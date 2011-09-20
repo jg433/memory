c676b8bbc9e9d7432d46ce19afc4d138b43edc41
jg433 <anarres@digifaktum.de>
2011-09-20 18:01:15 +0200

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memory::Application.config.secret_token = '68450cf1884473eab571f1e9416325c5fde6b0228244bdfae02f9be0d94017858c4ac83f52080aed444bce960c297f0680a065d72fdffd4ef4c24a221987f56a'
